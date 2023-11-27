1241f38441e9c6d70e79f03b056213e4918ea291
Ken-wangari <wangariken50@gmail.com>
2023-11-27 11:26:38 -0800

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_3_first = word[:3]
+word_2_last = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {}".format(word_3_first))
+print(f"Last 2 letters: {}".format(word_2_last))
+print(f"Middle word: {}".format(middle_word))
