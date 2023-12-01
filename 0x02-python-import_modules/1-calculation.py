9e19ace2bb8f8c9248c4f11cfa695e05ca2b9666
Ken-wangari <wangariken50@gmail.com>
2023-12-01 01:25:59 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+    """Print the sum, difference, product, and quotient of 10 and 5."""
+    
+    a = 10
+    b = 5
+    
+    # Addition
+    sum_result = a + b
+    print("{} + {} = {}".format(a, b, sum_result))
+    
+    # Subtraction
+    difference = a - b
+    print("{} - {} = {}".format(a, b, difference))
+    
+    # Multiplication
+    product = a * b
+    print("{} * {} = {}".format(a, b, product))
+    
+    # Division (with float result)
+    quotient = a / b
+    print("{} / {} = {}".format(a, b, quotient))
+
