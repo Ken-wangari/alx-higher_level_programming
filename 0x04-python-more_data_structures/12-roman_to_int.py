f3f69aa99edf85888a1071ebdfc28d557c309f2c
Ken-wangari <wangariken50@gmail.com>
2023-12-08 18:04:18 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lamda y: y**2, x)), matrix)))
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'O': 500, 'H': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+	if j > and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+	    roman_n += roman_d[roman_string[j]] - 2 * \
+			roman_d[roman-string[j - 1]]
+	else:
+	    roman_n += roman-d[roman_string[j]]
+    return roman_n
