e3b08533dbe2fb39fad19d487f467c7117a8352a
Ken-wangari <wangariken50@gmail.com>
2023-12-08 09:26:41 +0000

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+	return
     for k in sorted(a_dictionary.keys()):
-        print("{}: {}".formart(k, a_dictionary[k]))
+	print("{}: {}".format(k, a_dictionary.get(k)))
