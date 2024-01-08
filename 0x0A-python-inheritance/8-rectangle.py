9a8321366c5de976bac3d8bbd5ebfa8e63e2448e
Ken-wangari <wangariken50@gmail.com>
2024-01-08 12:48:21 -0800

    6-base_geometry.py 7-base_geometry.py 8-rectangle.py 9-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module for rectangle class.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''A subclass representing a recatngle.'''
+    def ___init__(self, width, height):
+        '''Constructor.'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module for rectangle class.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    '''A subclass rep a rectangle.'''
+    def ___init___(self, width, height):
+        '''Constructor.'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+       '''Method which returns area of rectangle.'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''string representation method.'''
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
