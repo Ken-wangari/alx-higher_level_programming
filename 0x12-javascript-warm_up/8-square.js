96aa7d27d8ff13fa7cdd6a898252b6ad9f8cdb9c
Ken-wangari <wangariken50@gmail.com>
2024-03-11 12:14:33 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+
+if (size === undefined || isNaN(size)) {
+  console.log('Missing size');
+} else {
+  const x = Number(size);
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
+
