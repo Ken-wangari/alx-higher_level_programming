fc14229071fed8e8f76631b11a24c5160af6f17c
Ken-wangari <wangariken50@gmail.com>
2024-03-11 12:20:02 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const numbers = process.argv.slice(2).map(Number);
+
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (const num of numbers) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && num < largest) {
+      secondLargest = num;
+    }
+  }
+
+  console.log(secondLargest !== -Infinity ? secondLargest : '0');
+}
+
