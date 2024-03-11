a4b06c573cbee6370effc37e11ddd9ceaf6866c9
Ken-wangari <wangariken50@gmail.com>
2024-03-11 11:06:25 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let line1 = "C is fun";
+let line2 = "Python is cool";
+let line3 = "JavaScript is amazing";
+
+function allLines() {
+    console.log("C is fun \nPython is cool \nJavaScript is amazing");
+}
+
+allLines();
