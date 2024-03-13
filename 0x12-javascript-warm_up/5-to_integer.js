8a050ad3ca730f93a54d78958022abbca5bbafb0
Ken-wangari <wangariken50@gmail.com>
2024-03-13 12:18:17 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-const inputNumber = process.argv[2];
-
-if (!inputNumber || isNaN(inputNumber)) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number:', parseInt(inputNumber));
+  console.log('My number:', parseInt(process.argv[2]));
 }
-
