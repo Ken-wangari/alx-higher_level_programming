53a9892d71ff9ebd62b8a6301e84aadee41c52a4
Ken-wangari <wangariken50@gmail.com>
2024-03-13 12:16:23 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-
-const numArguments = process.argv.length - 2;
-
-switch (numArguments) {
-  case 0:
-    console.log('No argument');
-    break;
-  case 1:
-    console.log('Argument found');
-    break;
-  default:
-    console.log('Arguments found');
-    break;
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-
