8c9a27dee67da787d99f9d29ee3ec1adf0dd37f7
Ken-wangari <wangariken50@gmail.com>
2024-03-15 10:53:41 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
+def get_states_by_name(username, password, database, state_name):
+    try:
+        db = MySQLdb.connect(host="localhost", user=username,
+                             passwd=password, db=database, port=3306)
+        cur = db.cursor()
+
+        query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+        cur.execute(query, (state_name,))
+
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 5:
+        print("Usage: python3 script_name.py <username> <password> <database> <state_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    get_states_by_name(username, password, database, state_name)
+
