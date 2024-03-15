cb7f6da261963d6ecb75935f7dce2fcb6c5ebb86
Ken-wangari <wangariken50@gmail.com>
2024-03-15 11:04:04 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,51 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Lists all cities from the database hbtn_0e_0_usa that belong to a given state."""
 import MySQLdb
 import sys
 
+def get_cities_by_state(username, password, database, state_name):
+    try:
+        # Establish connection to the database
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        cursor = db.cursor()
+
+        # Execute the SQL query with parameterization to avoid SQL injection
+        query = """
+            SELECT cities.name 
+            FROM cities 
+            INNER JOIN states ON states.id = cities.state_id 
+            WHERE states.name = %s
+        """
+        cursor.execute(query, (state_name,))
+
+        # Fetch all rows and extract city names
+        city_names = [row[0] for row in cursor.fetchall()]
+
+        # Print city names separated by commas
+        print(", ".join(city_names))
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        # Close cursor and database connection
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
-    rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
-    cur.close()
-    db.close()
+    # Check if correct number of command-line arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: python3 script_name.py <username> <password> <database> <state_name>")
+        sys.exit(1)
+
+    # Extract command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Call function to get cities by state
+    get_cities_by_state(username, password, database, state_name)
+
