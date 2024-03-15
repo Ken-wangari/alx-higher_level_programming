a5a9de4f4b06769bf771beef8b59a8e4a0a22a0a
Ken-wangari <wangariken50@gmail.com>
2024-03-15 01:43:20 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
