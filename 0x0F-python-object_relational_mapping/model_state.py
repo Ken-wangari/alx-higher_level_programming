f05e4414e01799c3ccbbd17751bd0edad615cdee
Ken-wangari <wangariken50@gmail.com>
2024-03-15 02:41:18 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-# Create a declarative base
 Base = declarative_base()
 
-# Define the State class
 class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(256), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
 
-if __name__ == "__main__":
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command-line arguments
-    username, password, database_name = sys.argv[1:]
-
-    try:
-        # Create an engine to connect to the database
-        engine = create_engine(
-            f'mysql+mysqldb://{username}:{password}@localhost/{database_name}',
-            pool_pre_ping=True
-        )
-
-        # Create the table if it doesn't exist
-        Base.metadata.create_all(engine)
-
-    except Exception as e:
-        # Handle errors
-        print("An error occurred:", e)
-        sys.exit(1)
+    def __repr__(self):
+        return "<State(id='%s', name='%s')>" % (self.id, self.name)
 
