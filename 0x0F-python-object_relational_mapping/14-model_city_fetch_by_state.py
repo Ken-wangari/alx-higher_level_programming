c19ac8487e28e09262f9934e10e488043f0d9b1c
Ken-wangari <wangariken50@gmail.com>
2024-03-22 10:24:27 -0700

    python object relational revisions

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,52 +1,19 @@
 #!/usr/bin/python3
-""" Prints the State object with the name passed as argument from the database """
-
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-def main():
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command-line arguments
-    username, password, database_name = sys.argv[1:]
-
-    try:
-        # Create an engine to connect to the database
-        engine = create_engine(
-            f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}'
-        )
-
-        # Bind the engine to the Base class
-        Base.metadata.bind = engine
-
-        # Create a session maker
-        Session = sessionmaker(bind=engine)
-
-        # Create a session
-        session = Session()
-
-        # Query the database to retrieve the state name and corresponding city details
-        query_result = session.query(State.name, City.id, City.name).join(City).all()
-
-        # Print the query result
-        for state_name, city_id, city_name in query_result:
-            print(f"{state_name}: ({city_id}) {city_name}")
-
-    except Exception as e:
-        # Handle errors
-        print("An error occurred:", e)
-        sys.exit(1)
-
-    finally:
-        # Close the session
-        session.close()
 
 if __name__ == "__main__":
-    main()
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,54 +1,17 @@
 #!/usr/bin/python3
-"""Lists all cities and their corresponding states from the database hbtn_0e_0_usa"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-def list_cities_and_states(username, password, database):
-    try:
-        # Connect to the database
-        db = MySQLdb.connect(host="localhost", user=username,
-                             passwd=password, db=database, port=3306)
-        
-        # Create a cursor object
-        cur = db.cursor()
-
-        # Execute the SQL query to fetch cities and their corresponding states
-        query = """
-            SELECT cities.id, cities.name, states.name 
-            FROM cities 
-            INNER JOIN states 
-            ON states.id = cities.state_id
-        """
-        cur.execute(query)
-
-        # Fetch all rows from the result set
-        rows = cur.fetchall()
-
-        # Print the fetched rows
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-
-    finally:
-        # Close cursor and database connection
-        if cur:
-            cur.close()
-        if db:
-            db.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 script_name.py <username> <password> <database>")
-        sys.exit(1)
-
-    # Extract command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Call the function to list cities and states
-    list_cities_and_states(username, password, database)
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
